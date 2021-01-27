682b0e1a6cb50c25f5169ca30b43ef9a8472c114
Mohamed Kari <contact@mkari.de>
2021-01-27 15:30:04 +0100

    add product implementation

--- retail.py
+++ retail.py
@@ -1,1 +1,15 @@
-print("This will be a retail example!")
+class Product: 
+  
+  # Constructor, wird ausgeführt, wenn die Klasse instanziiert wird
+  def __init__(self, name, tax_rate, net_price):
+    print("Product instantiated!")
+    self.name = name
+    self.tax_rate = tax_rate
+    self.net_price = net_priceiiwe
+
+  def compute_gross_price(self):
+    return round(self.tax_rate * self.net_price, 2)
+  
+  def describe(self):
+    print("This product is a", self.name, " of net price", 
+          self.net_price, "with tax rate", self.tax_rate)
